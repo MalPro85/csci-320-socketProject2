c341f2bb186327210feb332d2ef9f801efa4ebd3
MalPro85 <123274388+MalPro85@users.noreply.github.com>
2023-05-06 17:18:35 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -3,13 +3,14 @@ import os.path as path
 import sys
 
 IP = '127.0.0.1'  # change to the IP address of the server
-PORT = 12000  # change to a desired port number
+PORT = 14008  # change to a desired port number
 BUFFER_SIZE = 1024  # change to a desired buffer size
 
 
 def get_file_size(file_name: str) -> int:
     size = 0
     try:
+        # print(file_name)
         size = path.getsize(file_name)
     except FileNotFoundError as fnfe:
         print(fnfe)
@@ -20,10 +21,11 @@ def get_file_size(file_name: str) -> int:
 def send_file(filename: str, address: (str, int)):
     # get the file size in bytes
     # TODO: section 2 step 2
-
+    file_size = get_file_size(filename)
     # convert file_size to an 8-byte byte string using big endian
     # TODO: section 2 step 3
-
+    byte_size = file_size.to_bytes(8, byteorder='big')
+    byte_name = filename.encode()  # bytes(filename, 'utf-8')
     # create a TCP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -31,18 +33,32 @@ def send_file(filename: str, address: (str, int)):
         # TODO: section 2 step 5
         # send the file size in the first 8-bytes followed by the bytes
         # for the file name to server at (IP, PORT)
+        client_socket.connect(address)
         # TODO: section 2 step 6
+        client_socket.send(byte_size + byte_name)
         # TODO: section 2 step 7
-        # open the file to be transferred
-        with open(file_name, 'rb') as file:
-            # read the file in chunks and send each chunk to the server
-            is_done = False
-            while not is_done:
-                # TODO: section 2 step 8a
-                # TODO: section 2 step 8b
+        auth = client_socket.recv(8)
+        if auth == b'go ahead':
+            # open the file to be transferred
+            with open(file_name, 'rb') as file:
+                # read the file in chunks and send each chunk to the server
+                is_done = False
+                while not is_done:
+                    # TODO: section 2 step 8a
+                    chunk = file.read(16)
+                    print(chunk)
+                    # TODO: section 2 step 8b
+                    if len(chunk) > 0:
+                        client_socket.send(chunk)
+                    else:
+                        is_done = True
+        else:
+            raise OSError('Bad server response - was not go ahead!')
+
     except OSError as e:
         print(f'An error occurred while sending the file:\n\t{e}')
     finally:
+        print('successfully closing')
         client_socket.close()
 
 
@@ -57,6 +73,5 @@ if __name__ == "__main__":
         IP = sys.argv[2]
 
     send_file(file_name, (IP, PORT))
-
 
 
--- server.py
+++ server.py
@@ -3,12 +3,12 @@ import os
 import sys
 
 IP = '127.0.0.1'  # default IP address of the server
-PORT = 12000  # change to a desired port number
+PORT = 14008  # change to a desired port number
 BUFFER_SIZE = 1024  # change to a desired buffer size
 
 
 def get_file_info(data: bytes) -> (str, int):
-    return data[8:].decode(), int.from_bytes(data[:8],byteorder='big')
+    return data[8:].decode(), int.from_bytes(data[:8], byteorder='big')
 
 
 def upload_file(conn_socket: socket, file_name: str, file_size: int):
@@ -20,14 +20,19 @@ def upload_file(conn_socket: socket, file_name: str, file_size: int):
         try:
             while retrieved_size < file_size:
                 # TODO: section 1 step 6a
+                retrieved_chunk = conn_socket.recv(BUFFER_SIZE)
                 # TODO: section 1 stop 6b
+                retrieved_size += len(retrieved_chunk)
                 # TODO: section 1 stop 6c
+                file.write(retrieved_chunk)
         except OSError as oe:
             print(oe)
             os.remove(file_name)
 
+
 def start_server(ip, port):
     # create a TCP socket object
+    print(IP)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((ip, port))
     server_socket.listen(1)
@@ -38,9 +43,13 @@ def start_server(ip, port):
             (conn_socket, addr) = server_socket.accept()
             # TODO: section 1 step 2
             # expecting an 8-byte byte string for file size followed by file name
+            data = conn_socket.recv(BUFFER_SIZE)
+
             # TODO: section 1 step 3
+            (file_name, file_size) = get_file_info(data)
             print(f'Received: {file_name} with size = {file_size}')
             # TODO: section 1 step 4
+            conn_socket.send(b'go ahead')
             upload_file(conn_socket, file_name, file_size)
             conn_socket.close()
     except KeyboardInterrupt as ki:
@@ -49,9 +58,10 @@ def start_server(ip, port):
         server_socket.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # get IP address from cmd line
     if len(sys.argv) == 2:
         IP = sys.argv[1]  # IP from cmdline argument
 
+    print('here')
     start_server(IP, PORT)
